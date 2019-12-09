a44e8c8aa091a37ea85bd4ededb3b49dc3d9e36b
Jordan Machalek <jdm4344@rit.edu>
2019-12-09 12:08:54 -0500

    Set up keyboard based movement

--- Alt-Interfaces-Project3/Assets/Scripts/TurretControl.cs
+++ Alt-Interfaces-Project3/Assets/Scripts/TurretControl.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class TurretControl : MonoBehaviour
 {
+    // Attributes
+    [Header("Rotation")]
+    public Vector3 direction;
+    public float anglePerFrame;
+    public float totalRotation;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,26 @@ public class TurretControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Rotate();
+    }
+
+    //Rotates vehicle each frame depending on keyboard input
+    private void Rotate()
+    {
+        //Rotate left (positive)
+        if (Input.GetKey(KeyCode.A))
+        {
+            totalRotation += anglePerFrame;
+            direction = Quaternion.Euler(0, 0, anglePerFrame) * direction;
+        }
+        else if (Input.GetKey(KeyCode.D)) //Rotate right (negative)
+        {
+            totalRotation -= anglePerFrame;
+            direction = Quaternion.Euler(0, 0, -anglePerFrame) * direction;
+        }
+
+        //Update transform component
+        //transform.position = position;
+        transform.rotation = Quaternion.Euler(0, 0, totalRotation);
     }
 }
